a3c53799d1f46186ce82c8cd1f2ea482442f3148
RBDevScripts <itsritbis@gmail.com>
2024-09-12 19:47:45 +0530

    chat function

--- SiHproject/AiCareer/views.py
+++ SiHproject/AiCareer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from groq import Groq
 
 # Create your views here.
 def home(request):
@@ -11,3 +12,23 @@ def profile(request):
     return render(request, "profile.html")
 def home2(request):
     return render(request, "home2.html")
+
+def message(msg):
+    client = Groq(api_key="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")
+
+    prompt = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a career adviser only."
+            },
+            {
+                "role": "user",
+                "content": msg
+            }
+        ],
+    )
+    
+    ms = prompt.choices[0].message.content
+    return ms
